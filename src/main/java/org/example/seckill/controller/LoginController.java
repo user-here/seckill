4cb86afd97cc8d0ba072711123bf116d0e066c46
user-here <3088918760@qq.com>
2025-03-19 08:08:09 +0800

    新增登录功能

--- src/main/java/org/example/seckill/controller/LoginController.java
+++ src/main/java/org/example/seckill/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
      */
     @RequestMapping("/doLogin")
     @ResponseBody
-    public RespBean doLogin(@Valid LoginVo loginVo) {
+    public RespBean doLogin(LoginVo loginVo) {
         log.info("doLogin");
         return tUserService.doLogin(loginVo);
     }
